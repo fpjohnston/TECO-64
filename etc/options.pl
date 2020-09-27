32d9b297678fb45940b805f32dbaeab987adeda1
Franklin Pierce Johnston <fpjohnston@gmail.com>
2020-09-27 09:24:40 -0400

    Added copyright.

--- etc/options.pl
+++ etc/options.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #
-#  options.pl - Build-process tool for teco utility.
+#  options.pl - Build-process tool for TECO-64 text editor.
 #
 #  This script reads data from options.xml to determine command-line options and
 #  program information for the teco utility. This data is output to a C header
@@ -17,6 +17,26 @@
 #  3. A string defining the short command-line options (including whether they
 #     have optional or required arguments), used by the getopt_long() function.
 #  4. A table of long options, used by the getopt_long() function.
+#
+#  @copyright 2019-2020 Franklin P. Johnston / Nowwith Treble Software
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+# 
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+# 
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIA-
+#  BILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
 #
 ################################################################################
 
