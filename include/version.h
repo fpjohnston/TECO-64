da6373df2c1f3fd713bea06d1539dc70c985f63b
Franklin Pierce Johnston <fpjohnston@gmail.com>
2021-07-31 14:59:49 -0400

    Upgraded version number to 200.19.1.

--- include/version.h
+++ include/version.h
@@ -32,6 +32,6 @@
 
 #define MAJOR_VERSION    200    ///< Major version
 #define MINOR_VERSION     19    ///< Minor version
-#define PATCH_VERSION      0    ///< Patch version
+#define PATCH_VERSION      1    ///< Patch version
 
 #endif  // !defined(_VERSION_H)
