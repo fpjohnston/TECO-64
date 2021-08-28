20614d02a875e82ea94b1db2f198db2d9288faf5
Franklin Pierce Johnston <fpjohnston@gmail.com>
2021-08-28 09:12:41 -0400

    Updated version number to 200.20.0.

--- include/version.h
+++ include/version.h
@@ -31,7 +31,7 @@
 // TECO-64 version numbers
 
 #define MAJOR_VERSION    200    ///< Major version
-#define MINOR_VERSION     19    ///< Minor version
-#define PATCH_VERSION      4    ///< Patch version
+#define MINOR_VERSION     20    ///< Minor version
+#define PATCH_VERSION      0    ///< Patch version
 
 #endif  // !defined(_VERSION_H)
