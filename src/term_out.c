a4c4a57812f1a324b534c0effbc4b71cec06ae04
Franklin Pierce Johnston <fpjohnston@gmail.com>
2021-10-28 09:44:07 -0400

    Changed output of 8-bit characters (TECO-32 compatibility). Removed 8-bit characters embedded in test scripts.

--- src/term_out.c
+++ src/term_out.c
@@ -460,12 +460,52 @@ void type_out(int c)
                 break;
          }
     }
-    else if (f.et.eightbit)             // Can terminal display it?
+    else                                // Must be 8-bit character
     {
-        tputc(c, false);
-    }
-    else                                // No, make it printable
-    {
-        tprint("%s", table_8bit[c & 0x7f]);
+        // This table is used to ensure we print 8-bit characters like TECO-32.
+
+        static const char convert_table[] =
+        {
+            // 80 81 82 83 84 85 86 87 88 89 8A 8B 8C 8D 8E 8F
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            // 90 91 92 93 94 95 96 97 98 99 9A 9B 9C 9D 9E 9F
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            // A0 A1 A2 A3 A4 A5 A6 A7 A8 A9 AA AB AC AD AE AF
+                1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+            // B0 B1 B2 B3 B4 B5 B6 B7 B8 B9 BA BB BC BD BE BF
+                0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+            // C0 C1 C2 C3 C4 C5 C6 C7 C8 C9 CA CB CC CD CE CF
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            // D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 DA DB DC DD DE DF
+                1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+            // E0 E1 E2 E3 E4 E5 E6 E7 E8 E9 EA EB EC ED EE EF
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            // F0 F1 F2 F3 F4 F5 F6 F7 F8 F9 FA FB FC FD FE FF
+                1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+        };
+
+        // If the entry in the table above for the 8-bit character we want to
+        // output is non-zero, then that means we will print it in the form
+        // specified in table_8bit[].
+
+        bool convert_chr = convert_table[c & 0x7f] ? true : false;
+
+#if     !defined(__DECC)
+
+        // For any non-VMS environment, we will additionally use the ET flag to
+        // determine whether to do any special output.
+
+        convert_chr = convert_chr || !f.et.eightbit;
+
+#endif
+
+        if (convert_chr)
+        {
+            tprint("%s", table_8bit[c & 0x7f]);
+        }
+        else
+        {
+            tputc(c, false);
+        }
     }
 }
