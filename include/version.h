712f6109ca777e06e3c7319dd3b5650b34443943
Franklin Pierce Johnston <fpjohnston@gmail.com>
2022-05-01 10:58:34 -0400

    Updated version number to 200.28.0.

--- include/version.h
+++ include/version.h
@@ -33,8 +33,8 @@
 enum release_version                ///< TECO release version numbers
 {
     major_version = 200,
-    minor_version = 27,
-    patch_version = 2
+    minor_version = 28,
+    patch_version = 0
 };
 
 #endif  // !defined(_VERSION_H)
