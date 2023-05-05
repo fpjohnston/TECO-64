ab32af592afdb7a9a8c18e28c9daf7371cd5b11c
fpjohnston <nowwithtreble@gmail.com>
2023-05-05 08:20:24 -0400

    Fixed spacing for a couple of comments.

--- src/stubs.c
+++ src/stubs.c
@@ -127,7 +127,7 @@ void exec_FM(struct cmd *cmd)
 
     if (cmd->colon)
     {
-        store_val(FAILURE);     // Command failed
+        store_val(FAILURE);             // Command failed
     }
 }
 
@@ -145,7 +145,7 @@ void exec_FQ(struct cmd *cmd)
 
     if (cmd->colon)
     {
-        store_val(FAILURE);     // Command failed
+        store_val(FAILURE);             // Command failed
     }
 }
 
