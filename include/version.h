0a7b68c562dcbb0ba10887ddd3a15bbb725c48b7
fpjohnston <nowwithtreble@gmail.com>
2023-06-14 21:34:04 -0400

    Updated version number to 200.36.3.

--- include/version.h
+++ include/version.h
@@ -34,7 +34,7 @@ enum release_version                ///< TECO release version numbers
 {
     major_version = 200,
     minor_version = 36,
-    patch_version = 2
+    patch_version = 3
 };
 
 #endif  // !defined(_VERSION_H)
